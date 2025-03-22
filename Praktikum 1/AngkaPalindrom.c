d2eb3e8106d51763b5fac25f8b2aa2cff88c3899
Galton <francisgaltonnn@gmail.com>
2025-03-22 13:08:08 +0700

    Update AngkaPalindrom.c

--- Praktikum 1/AngkaPalindrom.c
+++ Praktikum 1/AngkaPalindrom.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int N, angka = 0, original, palindrom;
+
+    scanf("%d", &N);
+
+    original = N;  
+
+    while (N > 0) {
+        palindrom = N % 10;
+        angka = angka * 10 + palindrom;
+        N /= 10;
+    }
+
+    if (original == angka)
+        printf("1\n"); 
+    else
+        printf("0\n"); 
+
+    return 0;
+}
