9f15f42cfb7e9398f11d3222732343d4d3e106f4
Galton <francisgaltonnn@gmail.com>
2025-03-22 13:13:11 +0700

    Update MisiKebin.c

--- Praktikum 2/MisiKebin.c
+++ Praktikum 2/MisiKebin.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int prima(int angka) {
+    if (angka < 2) return 0; 
+    if (angka == 2) return 1;
+    if (angka % 2 == 0) return 0; 
+    for (int i = 3; i * i <= angka; i += 2) {
+        if (angka % i == 0) return 0; 
+    }
+    return 1; 
+}
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    
+    int result = 0;
+    for (int i = A; i <= B; i++) {
+        result += prima(i);
+    }
+    
+    printf("%d\n", result);
+    return 0;
+}
