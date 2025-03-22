b7715075b5395a412580e84f4295971a8115f968
Galton <francisgaltonnn@gmail.com>
2025-03-22 13:19:51 +0700

    Update KonversiSuhu.c

--- Praktikum 2/Pra Praktikum/KonversiSuhu.c
+++ Praktikum 2/Pra Praktikum/KonversiSuhu.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+    int N;
+    scanf("%d",&N);
+    for(int i = 0; i < N; i++){
+        double x, suhu;
+        char a, b;
+        scanf("%lf %c %c",&x,&a,&b);
+        if(a == 'C'){
+            if(b == 'F'){
+                suhu = x*9/5 + 32;
+            } else {
+                suhu = x + 273.15;
+            }
+        } else if(a == 'F'){
+            if(b == 'C'){
+                suhu = (x-32)*5/9;
+            }else {
+                suhu = (x-32)*5/9 + 273.15;
+            }
+        } else {
+            if(b == 'C'){
+                suhu = x - 273.15;
+            } else {
+                suhu = (x-273.15)*9/5 + 32;
+            }
+
+        }
+        suhu = floor(suhu);
+        printf("%.2lf\n",suhu);
+    }
+    return 0;
+}
