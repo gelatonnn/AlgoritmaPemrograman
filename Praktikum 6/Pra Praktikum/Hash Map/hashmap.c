6e36ea237e65b694e1c48848e1b962cf8afc6b44
Galton <francisgaltonnn@gmail.com>
2025-04-30 22:07:49 +0700

    Create hashmap.c

---
+++ Praktikum 6/Pra Praktikum/Hash Map/hashmap.c
@@ -0,0 +1,55 @@
+#include "hashmap.h"
+
+void CreateEmpty(HashMap *M) {
+    M->Count = Nil;
+    for (int i = 0; i < MaxEl; i++) {
+        M->Elements[i].Key = Undefined;
+        M->Elements[i].Value = Undefined;
+    }
+}
+
+address Hash(keytype K) {
+    return K % MaxEl;
+}
+
+valuetype Value(HashMap M, keytype k) {
+    address idx = Hash(k);
+    for (int i = 0; i < MaxEl; i++) {
+        int probeIdx = (idx + i) % MaxEl;
+        if (M.Elements[probeIdx].Key == k) {
+            return M.Elements[probeIdx].Value;
+        } else if (M.Elements[probeIdx].Key == Undefined) {
+            return Undefined;
+        }
+    }
+    return Undefined;
+}
+
+void Insert(HashMap *M, keytype k, valuetype v) {
+    if (M->Count == MaxEl) {
+        return;
+    }
+
+    if (Value(*M, k) != Undefined) {
+        return; 
+    }
+
+    address idx = Hash(k);
+    for (int i = 0; i < MaxEl; i++) {
+        int probeIdx = (idx + i) % MaxEl;
+        if (M->Elements[probeIdx].Key == Undefined) {
+            M->Elements[probeIdx].Key = k;
+            M->Elements[probeIdx].Value = v;
+            M->Count++;
+            return;
+        }
+    }
+}
+
+void printHashMap(HashMap M) {
+    for (int i = 0; i < MaxEl; i++) {
+        printf("%d:\n", i);
+        printf("  Key: %d\n", M.Elements[i].Key);
+        printf("  Value: %d\n", M.Elements[i].Value);
+    }
+}
