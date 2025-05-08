ac3cf965d646cc4aef03387e3b1ad01c42af4da3
Galton <francisgaltonnn@gmail.com>
2025-05-08 12:19:17 +0700

    Update Gabungan.c

--- Praktikum 7/Pra Praktikum/Gabungan.c
+++ Praktikum 7/Pra Praktikum/Gabungan.c
@@ -212,7 +212,11 @@
 //         int i = IDX_HEAD(q);
 //         while (i != IDX_TAIL(q)) {
 //             printf("%s, ", q.Tab[i].namaTim);
-//             i = (i + 1) % (IDX_MAX + 1);
+//             if (i == IDX_MAX){
+//                 i = 0;
+//             } else {
+//                 i++;
+//             }
 //         }
 //         printf("%s", q.Tab[i].namaTim);
 //     }
